67341d22f19b91a506987db023a0385259899887
liuxing3169 <liuxing3169@163.com>
2021-06-21 18:26:06 +0800

    use open edx database

--- cms/cms/settings/base.py
+++ cms/cms/settings/base.py
@@ -35,20 +35,20 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 DEBUG = False
 
-# CONFIG_FILE = get_env_setting('WAGTAIL_CFG')
+CONFIG_FILE = get_env_setting('WAGTAIL_CFG')
 
-# with codecs.open(CONFIG_FILE, encoding='utf-8') as f:
-#     __config__ = yaml.safe_load(f)
+with codecs.open(CONFIG_FILE, encoding='utf-8') as f:
+    __config__ = yaml.safe_load(f)
 
-#     # ENV_TOKENS and AUTH_TOKENS are included for reverse compatability.
-#     # Removing them may break plugins that rely on them.
-#     ENV_TOKENS = __config__
-#     AUTH_TOKENS = __config__
+    # ENV_TOKENS and AUTH_TOKENS are included for reverse compatability.
+    # Removing them may break plugins that rely on them.
+    ENV_TOKENS = __config__
+    AUTH_TOKENS = __config__
 
-#     # Add the key/values from config into the global namespace of this module.
-#     # But don't override the FEATURES dict because we do that in an additive way.
-#     __config_copy__ = copy.deepcopy(__config__)
-#     vars().update(__config_copy__)
+    # Add the key/values from config into the global namespace of this module.
+    # But don't override the FEATURES dict because we do that in an additive way.
+    __config_copy__ = copy.deepcopy(__config__)
+    vars().update(__config_copy__)
 
 
 # Application definition
@@ -119,28 +119,28 @@ WSGI_APPLICATION = 'cms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-   }
-}
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#   }
+#}
 
-# DATABASES = AUTH_TOKENS['DATABASES']
+DATABASES = AUTH_TOKENS['DATABASES']
 
-# # The normal database user does not have enough permissions to run migrations.  
-# # Migrations are run with separate credentials, given as DB_MIGRATION_*   
+# The normal database user does not have enough permissions to run migrations.  
+# Migrations are run with separate credentials, given as DB_MIGRATION_*   
 # # environment variables
-# for name, database in DATABASES.items():
-#     if name != 'read_replica':
-#          database.update({
-#              'ENGINE': os.environ.get('DB_MIGRATION_ENGINE', database['ENGINE']),
-#              'USER': os.environ.get('DB_MIGRATION_USER', database['USER']),
-#              'PASSWORD': os.environ.get('DB_MIGRATION_PASS', database['PASSWORD']),
-#              'NAME': os.environ.get('DB_MIGRATION_NAME', database['NAME']),
-#              'HOST': os.environ.get('DB_MIGRATION_HOST', database['HOST']), 
-#              'PORT': os.environ.get('DB_MIGRATION_PORT', database['PORT']), 
-#         })
+for name, database in DATABASES.items():
+    if name != 'read_replica':
+        database.update({
+             'ENGINE': os.environ.get('DB_MIGRATION_ENGINE', database['ENGINE']),
+             'USER': os.environ.get('DB_MIGRATION_USER', database['USER']),
+             'PASSWORD': os.environ.get('DB_MIGRATION_PASS', database['PASSWORD']),
+             'NAME': os.environ.get('DB_MIGRATION_NAME', database['NAME']),
+             'HOST': os.environ.get('DB_MIGRATION_HOST', database['HOST']), 
+             'PORT': os.environ.get('DB_MIGRATION_PORT', database['PORT']), 
+        })
 
 
 SECRET_KEY = 'v9q)jf$eyau4*wn@h#4xd@=ps+7m^ea6*515s3+6%m*&z_6%cd'
